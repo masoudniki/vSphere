e47452408abbc94a13a4529c2b0b9c161a0758af
masoud niki <masoud.niki79@gmail.com>
2021-07-26 12:18:56 +0430

    write test for vm api

--- src/Api/VM/ManageVms.php
+++ src/Api/VM/ManageVms.php
@@ -1,13 +1,15 @@
 <?php
     namespace FNDEV\vShpare\Api\VM;
 
+    use FNDEV\vShpare\Api\VM\Traits\CountAbleObject;
     use FNDEV\vShpare\Api\VM\Traits\IterableObject;
     use GuzzleHttp\Psr7\Response;
 
-    class ManageVms implements \Iterator {
-        use IterableObject;
-        private $items=[];
-        private $position=0;
+    class ManageVms implements \Iterator,\Countable {
+        use IterableObject,CountAbleObject;
+
+        private array $items = [];
+        private int $position = 0;
         public function __construct($vms,$HttpClient)
         {
             $this->parseObjects($vms,$HttpClient);
@@ -20,4 +22,6 @@
         public function first(){
             return (count($this->items) >= 1) ? $this->items[0] : null ;
         }
+
+
     }
--- tests/Unit/Modles/VM/VMTest.php
+++ tests/Unit/Modles/VM/VMTest.php
@@ -5,21 +5,30 @@ namespace FNDEV\Tests\Unit\Modles\VM;
 
 
 use FNDEV\Tests\TestCase;
+use FNDEV\vShpare\Api\VM\ManageVms;
 use FNDEV\vShpare\Api\VM\VM;
+use FNDEV\vShpare\Api\VM\VmSource;
 use GuzzleHttp\Psr7\Response;
 
 class VMTest extends TestCase
 {
     public VM $vm;
-    public function __setUp(){
+    public function setUp():void {
         parent::setUp();
         $this->vm=new VM($this->vmwareApiClient->getHttpClient());
     }
     public function test_get_vm_by_mo_id(){
-        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/vm.json')));
-        $response=$this->vm->byMoId('vm-111');
-        dd($response);
-
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixture/vm.json')));
+        $vmSource=$this->vm->byMoId('vm-111');
+        $this->assertInstanceOf(VmSource::class,$vmSource);
+        $this->assertEquals("vm-111",$vmSource->moid);
+        $this->assertLastRequestEquals("GET","/vm/vm-111");
+    }
+    public function test_get_all_vms(){
+        $this->mockHandler->append(new Response(200,[],file_get_contents(__DIR__.'/fixture/vms.json')));
+        $vmManager=$this->vm->all();
+        $this->assertInstanceOf(ManageVms::class,$vmManager);
+        $this->assertLastRequestEquals("GET","/vm");
     }
 
 }
